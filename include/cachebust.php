3b32177d198563df565f52f0f495ae01831ce5bd
Ibrahim Ame <ame.ibrahim@yahoo.com>
2024-09-23 11:23:36 +0300

    Cachebust Changes

--- include/cachebust.php
+++ include/cachebust.php
@@ -1,8 +1,8 @@
 <?php
     function cachebust($file_url = false) {
-        if (!file_exists($file_url)) {
-            return '8';
-        }
+        // if (!file_exists($file_url)) {
+        //     return '8';
+        // }
 
-        return filemtime($file_url);
+        return time();
     }
